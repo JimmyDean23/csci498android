ef47a2a3278b0f9819cca94886dfc74c4c6f3bdf
Billy Dixon <bidixon@mymail.mines.edu>
2012-08-29 23:32:56 -0600

    Added Restaurant class, modified layout according to tutorial 2

---
+++ LunchList/src/csci498/bidixon/lunchlist/Restaurant.java
@@ -0,0 +1,24 @@
+// Restaurant class contains information about a certain restaurant
+// such as name, address, etc.
+
+package csci498.bidixon.lunchlist;
+
+public class Restaurant {
+	
+	private String name = "";
+	private String address = "";
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
