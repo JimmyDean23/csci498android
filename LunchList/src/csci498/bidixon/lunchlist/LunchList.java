422487d90e37ae7350d9ea6fd37a77b3625fe277
Billy Dixon <bidixon@mymail.mines.edu>
2012-09-02 23:54:29 -0600

    Type is saved to restaurant model when user clicks save button

--- LunchList/src/csci498/bidixon/lunchlist/LunchList.java
+++ LunchList/src/csci498/bidixon/lunchlist/LunchList.java
@@ -20,13 +20,26 @@ public class LunchList extends Activity {
     }
     
     private View.OnClickListener onSave = new View.OnClickListener() {
-		
 		public void onClick(View v) {
 			EditText name = (EditText) findViewById(R.id.name);
 			EditText address = (EditText) findViewById(R.id.addr);
 			
 			r.setName(name.getText().toString());
 			r.setAddress(address.getText().toString());
+			
+			RadioGroup types = (RadioGroup) findViewById(R.id.types);
+			
+			switch (types.getCheckedRadioButtonId()){
+			case R.id.sit_down:
+				r.setType("sit_down");
+				break;
+			case R.id.take_out:
+				r.setType("take_out");
+				break;
+			case R.id.delivery:
+				r.setType("delivery");
+				break;
+			}
 		}
 	};
 
