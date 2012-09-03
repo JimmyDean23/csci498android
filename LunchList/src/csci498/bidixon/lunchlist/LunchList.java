ff171d5aab4337f38548e238b1b632a5ffd1339b
Billy Dixon <bidixon@mymail.mines.edu>
2012-09-03 00:47:39 -0600

    Changed single restaurant object to array list of restaurants

--- LunchList/src/csci498/bidixon/lunchlist/LunchList.java
+++ LunchList/src/csci498/bidixon/lunchlist/LunchList.java
@@ -1,5 +1,6 @@
 package csci498.bidixon.lunchlist;
 
+import java.util.*;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -9,7 +10,7 @@ import android.widget.*;
 
 public class LunchList extends Activity {
 	
-	Restaurant r = new Restaurant();
+	List<Restaurant> model = new ArrayList<Restaurant>();
 	private RadioGroup radioGroup4minute;
 	private RadioButton radioButtonJiyoon;
 	private RadioButton radioButtonJihyun;
@@ -78,20 +79,20 @@ public class LunchList extends Activity {
 			EditText name = (EditText) findViewById(R.id.name);
 			EditText address = (EditText) findViewById(R.id.addr);
 			
-			r.setName(name.getText().toString());
-			r.setAddress(address.getText().toString());
+			model.setName(name.getText().toString());
+			model.setAddress(address.getText().toString());
 			
 			RadioGroup types = (RadioGroup) findViewById(R.id.types);
 			
 			switch (types.getCheckedRadioButtonId()){
 			case R.id.sit_down:
-				r.setType("sit_down");
+				model.setType("sit_down");
 				break;
 			case R.id.take_out:
-				r.setType("take_out");
+				model.setType("take_out");
 				break;
 			case R.id.delivery:
-				r.setType("delivery");
+				model.setType("delivery");
 				break;
 			}
 		}
