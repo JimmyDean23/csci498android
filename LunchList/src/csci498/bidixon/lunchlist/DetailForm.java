efabfc95b069d629db7b8328f506ce620f01b3c6
Billy Dixon <bidixon@mymail.mines.edu>
2012-09-24 01:46:49 -0600

    Added getById to RestaurantHelper and added onDestory and load to DetailForm

--- LunchList/src/csci498/bidixon/lunchlist/DetailForm.java
+++ LunchList/src/csci498/bidixon/lunchlist/DetailForm.java
@@ -32,6 +32,33 @@ public class DetailForm extends Activity {
         Button saveButton = (Button) findViewById(R.id.save);
         saveButton.setOnClickListener(onSave);
         restaurantId = getIntent().getStringExtra(LunchList.ID_EXTRA);
+        
+        if(restaurantId != null){
+        	load();
+        }
+	}
+	
+	private void load() {
+		Cursor c = helper.getById(restaurantId);
+		c.moveToFirst();
+		name.setText(helper.getName(c));
+		address.setText(helper.getAddress(c));
+		notes.setText(helper.getNotes(c));
+		
+		if (helper.getType(c).equals("sit_down")) {
+			types.check(R.id.sit_down);
+		} else if (helper.getType(c).equals("take_out")) {
+			types.check(R.id.take_out);
+		} else {
+			types.check(R.id.delivery);
+		}
+		c.close();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		helper.close();
 	}
 	
 	private View.OnClickListener onSave = new View.OnClickListener() {
