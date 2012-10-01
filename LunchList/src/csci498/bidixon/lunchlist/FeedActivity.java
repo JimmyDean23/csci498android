fb199025a08ee240fcdd4b07e9bc5429bf31aafb
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-01 02:57:03 -0600

    Added FeedAdapter inner class to FeedActivity

--- LunchList/src/csci498/bidixon/lunchlist/FeedActivity.java
+++ LunchList/src/csci498/bidixon/lunchlist/FeedActivity.java
@@ -10,12 +10,18 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
 import org.mcsoxford.rss.RSSReader;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 /*
  * Activity that displays a restaurant's RSS feed 
@@ -68,6 +74,42 @@ public class FeedActivity extends ListActivity {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		
 		builder.setTitle("Exception!").setMessage(t.toString()).setPositiveButton("OK", null).show();
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		
+		RSSFeed feed = null;
+		
+		FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		
+		public int getCount() {
+			return feed.getItems().size();
+		}
+		
+		public Object getItem(int position) {
+			return feed.getItems().get(position);
+		}
+		
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			RSSItem item = (RSSItem) getItem(position);
+			
+			((TextView) row).setText(item.getTitle());
+			
+			return row;
+		}
 	}
 
 }
