0d2df9ec7ea417985871d136663384f1e8bec7dd
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-01 01:25:21 -0600

    Added onOptionsItemSelected in DetailForm, checks if there is a network connection using ConnectivityManager

--- LunchList/src/csci498/bidixon/lunchlist/DetailForm.java
+++ LunchList/src/csci498/bidixon/lunchlist/DetailForm.java
@@ -6,14 +6,19 @@
 package csci498.bidixon.lunchlist;
 
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.content.Intent;
 import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /*
  * Activity that allows user to create new Restaurant entries and save them to the database
@@ -95,6 +100,31 @@ public class DetailForm extends Activity {
 		return super.onCreateOptionsMenu(menu);
 	}
 	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.feed) {
+			if (isNetworkAvailable()) {
+				Intent i = new Intent(this, FeedActivity.class);
+				
+				i.putExtra(FeedActivity.FEED_URL, feed.getText().toString());
+				startActivity(i);
+			} else {
+				Toast.makeText(this, "Network Connection not available.", Toast.LENGTH_LONG).show();
+			}
+			
+			return true;
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
+	private boolean isNetworkAvailable() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo info = cm.getActiveNetworkInfo();
+		
+		return(info != null);
+	}
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
