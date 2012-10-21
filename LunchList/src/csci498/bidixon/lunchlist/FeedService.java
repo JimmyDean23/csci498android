1f618092411a73d938f442011d8bf7ec503a151f
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-20 23:28:22 -0600

    Added RSSReader to onHandleIntent

--- LunchList/src/csci498/bidixon/lunchlist/FeedService.java
+++ LunchList/src/csci498/bidixon/lunchlist/FeedService.java
@@ -5,10 +5,15 @@
 
 package csci498.bidixon.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL = "csci498.bidixon.lunchlist.EXTRA_URL";
 	
 	public FeedService() {
 		super("FeedService");
@@ -16,7 +21,13 @@ public class FeedService extends IntentService {
 	
 	@Override
 	public void onHandleIntent(Intent i) {
-		// do stuff
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		} catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 	
 }
