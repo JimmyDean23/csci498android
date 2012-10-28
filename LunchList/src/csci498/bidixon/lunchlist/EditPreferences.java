e8f69271047ea309b6e2b29941dc32441165653a
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-28 01:40:16 -0600

    Overrode onResume and onPause in EditPreferences

--- LunchList/src/csci498/bidixon/lunchlist/EditPreferences.java
+++ LunchList/src/csci498/bidixon/lunchlist/EditPreferences.java
@@ -5,20 +5,39 @@
 
 package csci498.bidixon.lunchlist;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 /*
  * Preference screen activity where users can change their preferences
  */
 public class EditPreferences extends PreferenceActivity {
 
+	SharedPreferences prefs;
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		prefs.registerOnSharedPreferenceChangeListener(onChange);
+	}
+	
+	@Override
+	public void onPause() {
+		prefs.unregisterOnSharedPreferenceChangeListener(onChange);
+		
+		super.onPause();
 	}
 	
 }
