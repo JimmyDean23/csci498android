e52f3051495e0f52ea7f04563cf9015e1e4b6cfd
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-28 00:54:36 -0600

    Added empty OnBootReceiver class

---
+++ LunchList/src/csci498/bidixon/lunchlist/OnBootReceiver.java
@@ -0,0 +1,20 @@
+/**
+ * @author Billy Dixon
+ * @version 0.1.9
+ */
+
+package csci498.bidixon.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+
+	}
+
+}
