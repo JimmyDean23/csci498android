f627c993f2a1b440ec3538a24652f06272407b65
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-27 23:32:42 -0600

    Created TimePrefenrence class

---
+++ LunchList/src/csci498/bidixon/lunchlist/TimePreference.java
@@ -0,0 +1,14 @@
+package csci498.bidixon.lunchlist;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class TimePreference extends DialogPreference {
+
+	public TimePreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+
+}
