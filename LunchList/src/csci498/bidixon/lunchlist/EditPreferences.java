1435be43930f6a0c023df779b1362fc91b764d45
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-28 01:55:20 -0600

    Added ChangleListener onChange to EditPreferences

--- LunchList/src/csci498/bidixon/lunchlist/EditPreferences.java
+++ LunchList/src/csci498/bidixon/lunchlist/EditPreferences.java
@@ -5,7 +5,9 @@
 
 package csci498.bidixon.lunchlist;
 
+import android.content.ComponentName;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -39,5 +41,31 @@ public class EditPreferences extends PreferenceActivity {
 		
 		super.onPause();
 	}
+	
+	SharedPreferences.OnSharedPreferenceChangeListener onChange = 
+			new SharedPreferences.OnSharedPreferenceChangeListener() {
+		
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+				String key) {
+			if ("alarm".equals(key)) {
+				boolean enabled = prefs.getBoolean(key, false);
+				int flag = (enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : 
+					PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
+				ComponentName component = new ComponentName(EditPreferences.this, OnBootReceiver.class);
+				
+				getPackageManager().setComponentEnabledSetting(component, flag, PackageManager.DONT_KILL_APP);
+				
+				if (enabled) {
+					OnBootReceiver.setAlarm(EditPreferences.this);
+				} else {
+					OnBootReceiver.cancelAlarm(EditPreferences.this);
+				}
+			} else if ("alarm_time".equals(key)) {
+				OnBootReceiver.cancelAlarm(EditPreferences.this);
+				OnBootReceiver.setAlarm(EditPreferences.this);
+			}
+		}
+		
+	};
 	
 }
