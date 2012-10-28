4babe0e1f8a7c8b4f8e8e1f1ca1dd6dd86a5f926
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-28 02:18:44 -0600

    Added AlarmActivity class

---
+++ LunchList/src/csci498/bidixon/lunchlist/AlarmActivity.java
@@ -0,0 +1,19 @@
+/**
+ * @author Billy Dixon
+ * @version 0.1.9
+ */
+
+package csci498.bidixon.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmActivity extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alarm);
+	}
+	
+}
