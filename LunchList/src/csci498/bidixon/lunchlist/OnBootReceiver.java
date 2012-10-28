54ac50c99a3aa65f7cdfcf7f56cf49b1d23b46d8
Billy Dixon <bidixon@mymail.mines.edu>
2012-10-28 01:30:16 -0600

    Added setAlarm and cancelAlarm to OnBootReceiver

--- LunchList/src/csci498/bidixon/lunchlist/OnBootReceiver.java
+++ LunchList/src/csci498/bidixon/lunchlist/OnBootReceiver.java
@@ -5,9 +5,14 @@
 
 package csci498.bidixon.lunchlist;
 
+import java.util.Calendar;
+
+import android.app.AlarmManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class OnBootReceiver extends BroadcastReceiver {
 
@@ -15,6 +20,29 @@ public class OnBootReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		
 
+	}
+	
+	public static void setAlarm(Context context) {
+		AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		Calendar calendar = Calendar.getInstance();
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		String time = prefs.getString("alarm_time", "12:00");
+		
+		calendar.set(Calendar.HOUR_OF_DAY, TimePreference.getHour(time));
+		calendar.set(Calendar.MINUTE, TimePreference.getMinute(time));
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		
+		if (calendar.getTimeInMillis() < System.currentTimeMillis()) {
+			calendar.add(Calendar.DAY_OF_YEAR, 1);
+		}
+		
+		manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, getPendingIntent(context));
+	}
+	
+	public static void cancelAlarm(Context context) {
+		AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		manager.cancel(getPendingIntent(context));
 	}
 
 }
