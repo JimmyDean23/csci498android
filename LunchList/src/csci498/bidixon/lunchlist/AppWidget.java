2f5c2ac6b7dd454c649e75f78b58fdd629746053
Billy Dixon <bidixon@mymail.mines.edu>
2012-11-08 00:11:36 -0700

    Added onUpdate in AppWidget to start the IntentService that will update the widget

--- LunchList/src/csci498/bidixon/lunchlist/AppWidget.java
+++ LunchList/src/csci498/bidixon/lunchlist/AppWidget.java
@@ -5,8 +5,14 @@
 
 package csci498.bidixon.lunchlist;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class AppWidget extends AppWidgetProvider {
-
+	@Override
+	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds) { 
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
 }
