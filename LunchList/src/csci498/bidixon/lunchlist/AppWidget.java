ee7f7a7b128f93aa21cdae41359da0482ee0dd94
Billy Dixon <bidixon@mymail.mines.edu>
2012-11-08 00:04:19 -0700

    Added empty AppWidget class

---
+++ LunchList/src/csci498/bidixon/lunchlist/AppWidget.java
@@ -0,0 +1,12 @@
+/**
+ * @author Billy Dixon
+ * @version 0.2.1
+ */
+
+package csci498.bidixon.lunchlist;
+
+import android.appwidget.AppWidgetProvider;
+
+public class AppWidget extends AppWidgetProvider {
+
+}
