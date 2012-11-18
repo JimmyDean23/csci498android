b11a08648b092ac41fd365cb5d69b2203e13d0e3
Billy Dixon <bidixon@mymail.mines.edu>
2012-11-18 02:54:41 -0700

    Added help page to onOptionsItemSelected in DetailFragment and LunchFragment

--- LunchList/src/csci498/bidixon/lunchlist/DetailFragment.java
+++ LunchList/src/csci498/bidixon/lunchlist/DetailFragment.java
@@ -132,6 +132,8 @@ public class DetailFragment extends Fragment {
 			
 			startActivity(i);
 			return true;			
+		} else if (item.getItemId() == R.id.help) {
+			startActivity(new Intent(getActivity(), HelpPage.class));
 		}
 		
 		return super.onOptionsItemSelected(item);
