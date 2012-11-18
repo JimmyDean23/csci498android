b055ed68fedcd038f8b57a2eabc3afeae5f57f34
Billy Dixon <bidixon@mymail.mines.edu>
2012-11-18 02:39:24 -0700

    Updated LunchList to add DetailFragment dynamically

--- LunchList/src/csci498/bidixon/lunchlist/LunchList.java
+++ LunchList/src/csci498/bidixon/lunchlist/LunchList.java
@@ -8,6 +8,8 @@ package csci498.bidixon.lunchlist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 /*
  * Main activity that displays a list of Restaurants in database
@@ -29,7 +31,24 @@ public class LunchList extends FragmentActivity implements LunchFragment.OnResta
     	if (findViewById(R.id.details) == null) {
     		Intent i = new Intent(this, DetailForm.class);
     		i.putExtra(ID_EXTRA, String.valueOf(id));
+    		
     		startActivity(i); 
+    	} else {    	
+    		FragmentManager fragmentManager = getSupportFragmentManager(); 
+    		DetailFragment details = (DetailFragment) fragmentManager.findFragmentById(R.id.details); 
+    		
+    		if (details == null) {
+    			details = DetailFragment.newInstance(id);
+    			
+    	    	FragmentTransaction xaction = fragmentManager.beginTransaction();
+    	    	
+    	    	xaction.add(R.id.details, details)
+    	    		.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+    	    		.addToBackStack(null)
+    	    		.commit();
+    		} else {
+    			details.loadRestaurant(String.valueOf(id));
+    		}
     	}
     }
     
