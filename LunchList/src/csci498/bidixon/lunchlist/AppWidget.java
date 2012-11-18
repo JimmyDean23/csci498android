ef79391d6582a6238d52219405899219aa040874
Billy Dixon <bidixon@mymail.mines.edu>
2012-11-17 22:26:39 -0700

    Implemented onHCUpdate to handle widgets on 3.0 and up

--- LunchList/src/csci498/bidixon/lunchlist/AppWidget.java
+++ LunchList/src/csci498/bidixon/lunchlist/AppWidget.java
@@ -5,15 +5,20 @@
 
 package csci498.bidixon.lunchlist;
 
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.widget.RemoteViews;
 
 /*
  * Widget for LunchList
  */
+@SuppressLint("NewApi")
 public class AppWidget extends AppWidgetProvider {
 	
 	@Override
@@ -23,6 +28,26 @@ public class AppWidget extends AppWidgetProvider {
 		} else {
 			context.startService(new Intent(context, WidgetService.class));
 		}
+	}
+	
+	public void onHCUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			Intent svcIntent = new Intent(context, ListWidgetService.class);
+			
+			svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]); 
+			svcIntent.setData( Uri.parse( svcIntent.toUri(Intent.URI_INTENT_SCHEME) ) );
+			
+			RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.widget);
+			widget.setRemoteAdapter(appWidgetIds[i], R.id.restaurants, svcIntent);
+			
+			Intent clickIntent = new Intent(context, DetailForm.class); 
+			PendingIntent clickPI = PendingIntent.getActivity(context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT); 
+			
+			widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+			appWidgetManager.updateAppWidget(appWidgetIds[i], widget); 
+		}
+		
+		super.onUpdate(context, appWidgetManager, appWidgetIds); 
 	}
 	
 }
---
+++ LunchList/src/csci498/bidixon/lunchlist/ListWidgetService.java
@@ -0,0 +1,20 @@
+/**
+ * @author Billy Dixon
+ * @version 0.2.2
+ */
+
+package csci498.bidixon.lunchlist;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+@SuppressLint("NewApi")
+public class ListWidgetService extends RemoteViewsService {
+
+	@Override
+	public RemoteViewsFactory onGetViewFactory(Intent intent) {
+		return (new ListViewsFactory(this.getApplicationContext(), intent));
+	}
+
+}
